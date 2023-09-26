0731f5cba4d0c097b0f93d7ab1b52742e475acdb
Isaac Tsibu-Darko <isaactsibudarko@gmail.com>
2023-09-26 17:36:44 +0000

    Initial setup and commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#161D31",
+        secondary: "#333333",
+        accentPink: "#E83E8C"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      width: {
+        '128': '32rem',
+        '130': '35.0652rem',
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
